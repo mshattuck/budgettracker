f9b4ca3535ac74cc9641a546ff16556c56741056
Mic Sha <shattuck.michael@gmail.com>
2020-10-27 16:17:05 -0400

    added service-worker.js code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,37 @@
 
 console.log("Hello from your service worker!");
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.js",
+    "manifest.webmanifest",
+    "styles.css",
+    "icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+
+    ];
+
+//install
+self.addEventListener("install", event =>
+{  
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => 
+{
+
+
+    
+});
+
+
+self.addEventListener("fetch", event => 
+{
+
+
+
+});
